33253bd582188e47b73c46c90f34d47dd4f7901c
Denise-E <denueichen@gmail.com>
2023-07-15 19:06:56 -0300

    Showing catgories name on client detail

--- crud/categorias.py
+++ crud/categorias.py
@@ -1,6 +1,24 @@
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
 import models.Categoria_Cliente as model
+import models.Categoria as model_categoria
 
 
 def get_categoriasPorCliente(db: Session,client_id: int):
-    return db.query(model.Categoria_Cliente).filter(model.Categoria_Cliente.id_cliente == client_id).all()
+    cat = db.query(model.Categoria_Cliente).filter(model.Categoria_Cliente.id_cliente == client_id).all()
+    finalList = []
+
+    for i in range(len(cat)):
+        print("categoriassss", cat[i].id_categoria) 
+        finalList.append({"categoria": getNombreCategoria(db,cat[i].id_categoria)})
+
+    return finalList
+    #Para acceder solo al nombre de la categoria, sin mostrar el id del cliente
+
+def getNombreCategoria(db: Session, category_id: int):
+    category = db.query(model_categoria.Categoria).filter(model_categoria.Categoria.id == category_id).first()
+    
+    if category is None:
+        raise HTTPException(status_code=404, detail="No existe la categoria buscada")
+
+    return category.nombre
