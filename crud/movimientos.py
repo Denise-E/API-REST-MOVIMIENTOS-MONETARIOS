c23c79c6b2381dae068db49875cf83946b906a9a
Denise-E <denueichen@gmail.com>
2023-07-16 15:47:15 -0300

    Adding comments on movements files

--- crud/movimientos.py
+++ crud/movimientos.py
@@ -1,35 +1,53 @@
 from fastapi import HTTPException
 from sqlalchemy.orm import Session
+
 import models.Movimiento as model
 from schemas import MovimientoSchema as schema
 from crud import cuentas as crud_accounts
 
+'''
+Busco movimiento por id en la base de datos. 
+Devuelve un único registro o None si no encuentra movimiento con el id solicitado
+'''
 def get_mov(db: Session, mov_id: int):
     return db.query(model.Movimiento).filter(model.Movimiento.id == mov_id).first()
 
+'''
+Busco todos los movimientos asociados a una misma cuenta, de la cual recibo el id. 
+Devuelve una lista con los resultados o None si no encuentra ningun movimiento asociado a la cuenta
+'''
 def get_movementsByAccount(db: Session, account_id: int):
     return db.query(model.Movimiento).filter(model.Movimiento.id_cuenta == account_id).all()
 
+'''
+Creación de un movimiento, Ingreso o Egreso de dinero. 
+Creando el registro en la base de datos en la tabla "movimientos" en caso de corresponder.
+Todo ingreso se registra y en caso de los egresos solo si alcanza el saldo en la cuenta del cliente.
+'''
 def create_mov(db: Session, data:schema.MovimientoCreate):
     #Valido en caso que quieran hacer un egreso que alcance el saldo de la cuenta
     if data.tipo == 2:
         saldo = crud_accounts.get_clientBalance(db, data.id_cuenta) #Rutilizo el metodo ya creado
         saldo = saldo.saldo_ARS
 
-    if (saldo - data.importe) < 0:
+    if (saldo - data.importe) < 0: #Si no alcanza para retirar el monto deciado lanzo Excepción con status 404
         raise HTTPException(status_code=404, detail="Saldo insuficiente")
 
+    #Si alcanzó el saldo para el egreso o estan solicitando un ingreso creo el registro.
     new_mov = model.Movimiento(id_cuenta = data.id_cuenta, tipo = data.tipo, importe = data.importe, fecha = data.fecha)
     db.add(new_mov)
     db.commit()
     db.refresh(new_mov)
     return new_mov
 
+#Eliminación de un movimiento segun su id
 def delete_mov(db: Session, mov_id: int):
-    mov = get_mov(db, mov_id=mov_id)
+    mov = get_mov(db, mov_id=mov_id) #Verifico que exista tal movimiento
 
-    if mov is not None:
+    if mov is not None: #Si se encontró un movimiento con el id solicitado lo elimino.
         db.delete(mov)
         db.commit()
-        
+
+    #En caso de retornarse None se lanza una excepción desde el router    
     return mov
+    
--- router/movimiento.py
+++ router/movimiento.py
@@ -18,29 +18,34 @@ def get_db():
         db.close()
 
 
+#Ruta para acceder por URL al detalle del movimiento con id especificado en la url, en caso de existir.
 @movimiento.get("/movimientos/{mov_id}",response_model=schema.Movimiento) 
 def read_mov(mov_id: int, db: Session = Depends(get_db)):
-    db_mov = crud.get_mov(db, mov_id=mov_id)
+    db_mov = crud.get_mov(db, mov_id=mov_id) #Busco el movimiento por ID
 
-    if db_mov is None:
+    if db_mov is None: #Si no existe un movimiento con el id solicitado lanzo excepción con status 404
         raise HTTPException(status_code=404, detail="Movimiento no encontrado")
     
-    return db_mov
+    return db_mov #Si exitió un movimiento, no se lanzó la excepción y este se verá en el navegador.
 
+
+#Ruta para la creación de un movimiento (Ingreso o Egreso de dinero a una cuenta)
 @movimiento.post("/movimientos",response_model=schema.MovimientoCreate)
 def create_mov(input: schema.MovimientoCreate,db: Session = Depends(get_db)):
-    new_mov = crud.create_mov(db, input)
+    new_mov = crud.create_mov(db, input) #Creo el movimiento
 
-    if new_mov is None:
+    if new_mov is None: #Si me retornó None, no se pudo efectuar el movimiento y lanzo excepción avisandolo.
         raise HTTPException(status_code=404, detail="No se pudo registrar el movimiento")
     
-    return new_mov
+    return new_mov #Si se creó, muestro el registro creado.
 
+
+#Ruta para eliminar un movimiento por su id
 @movimiento.delete("/movimientos/{mov_id}",response_model=schema.Movimiento)
 def delete_mov(mov_id: int, db: Session = Depends(get_db)):
-    db_mov = crud.delete_mov(db, mov_id=mov_id)
+    db_mov = crud.delete_mov(db, mov_id=mov_id) #Elimino el movimiento
 
-    if db_mov is None:
+    if db_mov is None: #Si no pudo eliminarse, db_mov valdrá none e informe el error con una excepción.
         raise HTTPException(status_code=404, detail="Movimiento no encontrado")
 
-    return db_mov
+    return db_mov #Si se logró la accioón deseada, devuelvo el movimiento eliminado
