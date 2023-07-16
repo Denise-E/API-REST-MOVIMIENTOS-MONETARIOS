3bee1ee9ef02d9db504669bf17dd196e49b9b398
Denise-E <denueichen@gmail.com>
2023-07-16 17:26:53 -0300

    Adding comments

--- crud/categorias.py
+++ crud/categorias.py
@@ -3,49 +3,56 @@ from sqlalchemy.orm import Session
 import models.Categoria_Cliente as model
 import models.Categoria as model_categoria
 
-
+#Obtengo todas las categorias de un cliente, buscando por su id
 def get_categoriesByClient(db: Session,client_id: int):
     return db.query(model.Categoria_Cliente).filter(model.Categoria_Cliente.id_cliente == client_id).all()
     
 
+#Devuelve una lista con los nombres de las categorias a las cuales pertenece un cliente
 def get_categoriesByClient_detail(db: Session,client_id: int):
-    cat = get_categoriesByClient(db,client_id)
-    finalList = []
+    cat = get_categoriesByClient(db,client_id) #Obtengo todas las categorias
+    finalList = [] #Creo lista auxiliar, la cual estará devolviendo la función
     
-    #Para acceder solo al nombre de la categoria, sin mostrar el id del cliente
+    #Para acceder solo al nombre de la categoria, sin mostrar el id del cliente que me llega en el objeto
     for i in range(len(cat)): 
         finalList.append(getCategoryName(db,cat[i].id_categoria))
 
 
     return finalList
 
+#Obtengo la categoria correspondiente con el numero de id pasado por parametro
 def get_categoryById(db: Session,category_id: int):
     category = db.query(model_categoria.Categoria).filter(model_categoria.Categoria.id == category_id).first()
     
-    if category is None:
+    if category is None: #Si no existe la categoria lo informo
         raise HTTPException(status_code=404, detail="No existe la categoria buscada")
     
-    return category
+    return category #Si existe la categoria la devuelvo
 
 
+#Devuelve el nombre de la categoria cuyo id pasaron por parametro
 def getCategoryName(db: Session, category_id: int):
-    category = get_categoryById(db,category_id)
-    return category.nombre
+    category = get_categoryById(db,category_id) #Busco la categoria por id
+    return category.nombre #Devuelvo únicamnete el nombre de la categoria encontrada
+    #Si la categoria no se encontró el metodo llamado en la linea 34 lanza una excepción, por lo cual no se ejecutará el return
 
+'''
+Elimina todas las asociaciones de un cliente con sus categorias. 
+Todos los registros de ese cliente de la tabla intermeda categorias_cliente
+'''
 def delete_clientCategories(db: Session, client_id: int):
-    categories = get_categoriesByClient(db,client_id)
+    categories = get_categoriesByClient(db,client_id) #Obtengo todas las categorias del cliente
 
-    if categories is not None:
+    if categories is not None: #Si hay registros elimino cada uno de estos y persisto los cambios en la base de datos
         for i in range(len(categories)):
             db.delete(categories[i])
             db.commit()
 
     return categories
 
-'''
-Si existe el cliente, desde categorias valido id de la catgeoria valido y que el ususario no este 
-asociado ya a esa categoria
-'''
+
+#Valida que el id de la categoria sea válido y que el usuario no este asociado ya a esa categoria
+#Este metodo es usado al agregar al cliente a una categoria
 def validateCategoryForClient(db: Session, client_id:int, cat_id: int):
     get_categoryById(db,cat_id) #Verifica que exista una categoria con el id solicitado, si no la hay lanza una excepcion
 
--- crud/cuentas.py
+++ crud/cuentas.py
@@ -5,15 +5,17 @@ from schemas import CuentaSchema as schema
 from crud import movimientos as crud_movimientos
 import requests
 
-
+#Obtengo todas las cuentas del cliente cuyo id pasaron por parametro
 def get_accountsByClient(db: Session,client_id: int):
     return db.query(model.Cuenta).filter(model.Cuenta.id_cliente == client_id).all()
 
+#Obtengo una cuenta por su id
 def get_accountById(db: Session,account_id: int):
     return db.query(model.Cuenta).filter(model.Cuenta.id == account_id).first()
 
+#Devuelve una lista con el id de todas las cuentas asociadas al cliente cuyo id pasaron por parametro
 def get_accountsByClient_detail(db: Session,client_id: int):
-    account = get_accountsByClient(db, client_id)
+    account = get_accountsByClient(db, client_id) #Obtiene todas las cuentas del cliente
     finalList = [{"IDs": []}]
 
     #Para acceder solo al id de la cuenta, sin mostrar el id del cliente.
@@ -22,12 +24,13 @@ def get_accountsByClient_detail(db: Session,client_id: int):
 
     return finalList 
     
-
+#Elimina todas las cuentas asociadas a un cliente
 def delete_clienteAccounts(db: Session, client_id: int):    
-    accounts = get_accountsByClient(db, client_id)
+    accounts = get_accountsByClient(db, client_id) #Busco todas las cuentas del cliente por el id pasado por parametro
 
     if accounts is not None:
-        for i in range(len(accounts)):
+        for i in range(len(accounts)): #Recorro la lista con las cuentas
+            #Voy eliminando de a una y persistiendo los cambios en la base de datos
             db.delete(accounts[i])
             db.commit()
 
@@ -36,9 +39,9 @@ def delete_clienteAccounts(db: Session, client_id: int):
  
 #Devuelve un objeto con el saldo final de la cuenta al día de la fecha, tanto en pesos como en dolares
 def get_clientBalance(db: Session, account_id: int):
-    account = get_accountById(db, account_id)
+    account = get_accountById(db, account_id) #Obtiene una cuenta por su id
 
-    if account is None:
+    if account is None: #Si no existe una cuenta con el id buscado lo informa
         raise HTTPException(status_code=404, detail="No existe una cuenta con el ID solicitado")
 
     #Obtengo todos los movimientos de la cuenta solicitada por id
@@ -50,20 +53,21 @@ def get_clientBalance(db: Session, account_id: int):
     if movements is not None:
         for i in range(len(movements)):
             if movements[i].tipo == 1:
-                totalARS += movements[i].importe #Ingreso
+                totalARS += movements[i].importe #Ingreso. Sumo el importe registrado a totalArs
             else: 
-                totalARS -= movements[i].importe #Egreso
+                totalARS -= movements[i].importe #Egreso. Resto el importe registrado a totalArs
         
-        totalUSD = get_total_usd(totalARS) #Llamo al metodo que me va a obtener el valor en USD segun la cotización solicitada
+        totalUSD = get_total_usd(totalARS) #Llamo al método que me va a obtener el valor en USD segun la cotización solicitada
 
-    clientBalance = schema.CuentaSaldo(saldo_ARS= totalARS, saldo_USD= totalUSD) #Armo el objeto que va a ser mostrado en la URL
+    clientBalance = schema.CuentaSaldo(saldo_ARS = totalARS, saldo_USD = totalUSD) #Armo el objeto que va a ser mostrado en el navegador
     return clientBalance
 
+#Recibe el saldo total de la cuenta en pesos y devuelve el valor en dólares consumiendo la API con la cotización actualizada.
 def get_total_usd(totalARS: float):
     usd_value = 0
 
     response_API = requests.get('https://www.dolarsi.com/api/api.php?type=valoresprincipales')
-    data = response_API.json() #Accedo a todo el array que contiene la api en formato json para poder trabaharlo
+    data = response_API.json() #Accedo a todo el array que contiene la api en formato json para poder trabajarlo
     
     #Busco el objeto dentro de la lista data con nombre = Dolar Bolsa
     cont = 0
@@ -72,11 +76,11 @@ def get_total_usd(totalARS: float):
     while encontrado is False and cont < len(data):
         if data[cont]["casa"]["nombre"] == "Dolar Bolsa":
             encontrado = True
-            usd_value = data[cont]["casa"]["venta"] #Me guardo la cotizacion de venta en una variable
+            usd_value = data[cont]["casa"]["venta"] #Me guardo la cotizacion del dolar venta en una variable
 
         cont+=1
 
-    #Formateando usd_value que llega como string con coma para poder transformarlo a floaT
+    #Formatea usd_value que llega como string con coma para poder transformarlo a float
     float_usd = usd_value.replace(",", ".")
     #print("USD" , float_usd)
 
